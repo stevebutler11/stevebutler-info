f3f67a0d3163bdd093e695f462d37c72fe11707d
steve <stevebutler11@gmail.com>
2024-09-27 11:35:37 +0100

    sm links increased in size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
     <footer className=" flex flex-col justify-center items-center h-24">
       <div className="flex flex-row">
         <Link href="https://github.com/stevebutler11" target="_blank">
-          <GitHubLogoIcon className="h-8 w-8 m-1" />
+          <GitHubLogoIcon className="h-12 w-12 m-1" />
         </Link>
 
         <Link href="https://www.linkedin.com/in/steven-butler-3749a6a4/" target="_blank">
-          <LinkedInLogoIcon className="h-8 w-8 m-1" />
+          <LinkedInLogoIcon className="h-12 w-12 m-1" />
         </Link>
       </div>
     </footer>
